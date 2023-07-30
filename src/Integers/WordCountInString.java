c97a4ca80d8b92bb119413a5629b301ffb171122
khushbu07 <sarnot123@gmail.com>
2023-07-30 09:45:43 +0530

    Add files via upload
    directly

---
+++ src/Integers/WordCountInString.java
@@ -0,0 +1,29 @@
+package Array;
+
+public class WordCountInString {
+
+	
+	 void countWord(String input) {
+		String arr[] = input.split(" ");
+		for (int i = 0; i < arr.length; i++) {
+			int count = 0;
+			String word = arr[i];
+			if (input.contains(word)) {
+				for (int j = 0; j < arr.length; j++) {
+					if (word.equals(arr[j]))
+						count++;
+			}
+			System.out.println(word+" --> "+count);
+			input = input.replaceAll(word," ");
+			
+			}
+			
+	}
+	 }
+	public static void main(String[] args) {
+		WordCountInString wc=new WordCountInString();
+		String str = "Hi Hello gm gn Hi Hello gm gm gn techno";
+		wc.countWord(str);
+	
+	}
+}
