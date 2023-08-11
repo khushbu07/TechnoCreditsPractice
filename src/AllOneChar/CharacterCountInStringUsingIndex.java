d33ee7c495c3a9e28bbce62a6a42f268b77a4b7d
khushbu07 <sarnot123@gmail.com>
2023-08-12 00:20:17 +0530

    Update CharacterCountInStringUsingIndex.java

--- src/AllOneChar/CharacterCountInStringUsingIndex.java
+++ src/AllOneChar/CharacterCountInStringUsingIndex.java
@@ -4,22 +4,25 @@ public class CharacterCountInStringUsingIndex {
 
 	public static void main(String[] args) {
 
-		// using indexof to overcome the repeatations
+			
+    	//using indexof we overcome the repetition
+    	String str="Monalikaalioooooooookk";
+    	
+    	
+    	for(int i=0;i<str.length();i++) {
+    		int count=0;
+    		char ch=str.charAt(i);
+    		if(str.indexOf(ch)!=i)
+    	       break;		
+    		for(int j=0;j<str.length();j++) {
+    			if(str.charAt(j)==ch)
+    				count++;
+    		}
+    		
+    		System.out.println(ch+":"+count);
 
-		String str = "Monkkkkiii"; // "Monaliiikakk"
-
-		for (int i = 0; i < str.length(); i++) {
-			int count = 0;
-			char ch = str.charAt(i);
-			if (str.indexOf(ch) != i)
-				break;
-			for (int j = 0; j < str.length(); j++) {
-				if (ch == str.charAt(j))
-					count++;
-			}
-
-			System.out.println(ch + " : " + count);
-		}
-
+    	}
+    }
 	}
-}
+
+
