b978f2988424aafc5aa78c3c7389d1d99c6f0128
khushbu07 <sarnot123@gmail.com>
2023-08-22 00:10:44 +0530

    customException

---
+++ src/CustomException/AgeNotException.java
@@ -0,0 +1,14 @@
+package CustomException;
+
+public class AgeNotException extends Exception {
+	
+	AgeNotException(){
+		super();
+	}
+	
+	AgeNotException(String message){
+		super(message);
+	}
+	
+
+}
---
+++ src/CustomException/Pub.java
@@ -0,0 +1,23 @@
+package CustomException;
+
+public class Pub {
+
+	void validateEntry(int age) throws Exception {
+		if (age <= 18) {
+
+			throw new AgeNotException();
+		}
+
+	}
+
+	public static void main(String[] args) {
+
+		try {
+			new Pub().validateEntry(15);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
+	}
+
+}
