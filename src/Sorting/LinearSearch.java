ea3b8d92310fbd4e23227397d0e44b4136333582
khushbu07 <sarnot123@gmail.com>
2023-09-17 16:44:21 +0530

    search progm

---
+++ src/Sorting/LinearSearch.java
@@ -0,0 +1,23 @@
+package Sorting;
+
+public class LinearSearch {
+
+	public static void main(String[] args) {
+
+		int a[] = { 12, 14, 50, 40, 23 };
+		int searchKey=45;
+		boolean flag=false;
+		 for(int i=0;i<a.length;i++){
+			 if(a[i]==searchKey){
+				 System.out.println("Element fount at index position :"+i);
+				 flag=true;
+				 break;
+			 }
+		 }
+
+		if(flag==false)
+			 System.out.println("Element not found in given Array");
+		 
+	}
+
+}
