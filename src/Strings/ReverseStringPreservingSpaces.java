941c0ab1fd2200844fd213631b061c1f16cdf777
khushbu07 <sarnot123@gmail.com>
2025-03-02 23:37:44 +0530

    prep

---
+++ src/Strings/ReverseStringPreservingSpaces.java
@@ -0,0 +1,39 @@
+package Strings;
+
+public class ReverseStringPreservingSpaces {
+
+	public static void main(String[] args) {
+		
+		String str="I Am Not String";
+		
+		//"gni rtS toNmAI"
+		
+		String res=reverseStringPreservingSpaces(str);
+		System.out.println(res);
+
+     }
+	
+	public static String reverseStringPreservingSpaces(String input) {
+		char str[]=input.toCharArray();
+		 int i=0;
+		 int j=str.length-1;
+		 while(i<j) {
+			 if(str[i]==' ') {
+				 i++;
+			 }
+			 else if (str[j]==' ') {
+				 j--;
+			 }
+			 else {
+				 char temp=str[i];
+				 str[i]=str[j];
+				 str[j]=temp;
+				 i++;
+				 j--;
+			 }
+		 }
+		 
+		return new String(str);
+	}
+
+}
